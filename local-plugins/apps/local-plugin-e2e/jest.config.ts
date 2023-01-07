300a416e940a3a247872018c385bca664d0f7202
Zack DeRose <zack.derose@gmail.com>
2023-01-06 18:12:20 -0700

    created local-plugin project

---
+++ local-plugins/apps/local-plugin-e2e/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'local-plugin-e2e',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/local-plugin-e2e',
+};
