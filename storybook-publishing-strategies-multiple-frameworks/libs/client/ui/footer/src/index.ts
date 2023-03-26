68f76ed16d7469a2ad1a29ad264419f785143bbd
ids <idsmesales@gmail.com>
2023-03-26 05:18:56 +0200

    fix(testing): tried running npm run test but did not work as component content had changed.

--- storybook-publishing-strategies-multiple-frameworks/libs/client/ui/footer/src/index.ts
+++ storybook-publishing-strategies-multiple-frameworks/libs/client/ui/footer/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/client-ui-footer.module';
+export * from './lib/footer/footer.component';
--- storybook-publishing-strategies-multiple-frameworks/libs/client/ui/header/src/index.ts
+++ storybook-publishing-strategies-multiple-frameworks/libs/client/ui/header/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/client-ui-header.module';
+export * from './lib/header/header.component';
