fe3c9d40811c34e722e96ab6a1eca2bec16b65a5
Caleb Ukle <caleb.ukle+github@pm.me>
2023-05-30 10:04:52 -0500

    docs

--- fastify-redis/libs/utils-redis-query/src/lib/utils-redis-query.ts
+++ fastify-redis/libs/utils-redis-query/src/lib/utils-redis-query.ts
@@ -1,16 +1,22 @@
 import { createHash } from 'node:crypto';
 
+/**
+ * add the id back to the result object
+ **/
 export function mapIdToHash<T = unknown>(
-  results: Array<[any, T]>,
+  results: Array<[unknown, T]>,
   keys: string[]
 ) {
-  // TODO(caleb): type this better
-  return results.map((res: [T, any], idx) => ({
+  return results.map((res: [number, T], idx) => ({
     ...res[1],
     id: keys[idx].split(':').at(-1),
   }));
 }
 
+/**
+ * Sample function for creating ids.
+ * A more robust system should most likely be used, Especially if uniqueness is required
+ **/
 export function createId(value: Record<string, string | number>) {
   const content = Object.values(value)
     .map((v) => v.toString())
