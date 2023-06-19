9eb7512d6d14c1e027e57b302621d3d5d374fd8b
Isaac Mann <isaacplmann@users.noreply.github.com>
2023-06-19 16:17:21 -0400

    Migrate action (#23)
    * add migrate github action
    
    * trigger on PR
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * action update
    
    * update e2e workflow
    
    * Add note in contributing guide
    
    * Fail e2e script when e2es fail

---
+++ e2e.js
@@ -0,0 +1,40 @@
+const { execSync } = require("child_process");
+const { readdirSync } = require("fs");
+
+const BROKEN_RECIPES = [".git", ".github", "deno-deploy"];
+
+function installPackages(cwd) {
+  const files = readdirSync(cwd);
+  if (files.includes("pnpm-lock.yaml")) {
+    execSync("pnpm i", { cwd });
+  } else if (files.includes("yarn-lock.json")) {
+    execSync("yarn", { cwd });
+  } else {
+    execSync("npm i --legacy-peer-deps", { cwd });
+  }
+}
+
+function runE2eTests(cwd) {
+  execSync("CI=true npx nx run-many --target=e2e --parallel=false", { cwd, stdio: [0,1,2] });
+}
+
+function processAllExamples() {
+  const files = readdirSync(".", { withFileTypes: true });
+  const failures = [];
+  files.forEach((file) => {
+    if (file.isDirectory() && !BROKEN_RECIPES.includes(file.name)) {
+      const cwd = "./" + file.name;
+      console.log(cwd);
+      try {
+        installPackages(cwd);
+        runE2eTests(cwd);
+      } catch (ex) {
+        failures.push(ex);
+      }
+    }
+  });
+  if (failures.length > 0) {
+    throw new Error('E2E Tests Failed');
+  }
+}
+processAllExamples();
---
+++ migrate.js
@@ -0,0 +1,51 @@
+const { execSync } = require("child_process");
+const { readdirSync } = require("fs");
+
+const BROKEN_RECIPES = [".git", ".github"];
+
+function installPackages(cwd) {
+  console.log("Installing packages for " + cwd);
+  const files = readdirSync(cwd);
+  if (files.includes("pnpm-lock.yaml")) {
+    execSync("pnpm i", { cwd, stdio: [0, 1, 2] });
+  } else if (files.includes("yarn.lock")) {
+    execSync("yarn", { cwd, stdio: [0, 1, 2] });
+  } else {
+    execSync("npm ci --legacy-peer-deps", { cwd, stdio: [0, 1, 2] });
+  }
+}
+function migrateToLatest(cwd) {
+  console.log(`Migrating ${cwd}...`);
+  execSync("CI=true npx nx migrate latest", { cwd, stdio: [0, 1, 2] });
+  execSync("CI=true npx nx migrate --run-migrations --no-interactive", {
+    cwd,
+    stdio: [0, 1, 2],
+    timeout: 60000,
+  });
+  execSync("rm -rf migrations.json", { cwd, stdio: [0, 1, 2] });
+  console.log(`Done migrating ${cwd}.`);
+}
+
+function processAllExamples() {
+  const files = readdirSync(".", { withFileTypes: true });
+  let failedMigrations = [];
+  files.forEach((file) => {
+    if (file.isDirectory() && !BROKEN_RECIPES.includes(file.name)) {
+      const cwd = "./" + file.name;
+      installPackages(cwd);
+      try {
+        migrateToLatest(cwd);
+      } catch (ex) {
+        console.log(ex);
+        console.log("Continuing to next example...");
+        failedMigrations.push(cwd);
+      }
+    }
+  });
+  if (failedMigrations.length > 0) {
+    console.log(
+      "The following migrations failed: " + failedMigrations.join(", ")
+    );
+  }
+}
+processAllExamples();
