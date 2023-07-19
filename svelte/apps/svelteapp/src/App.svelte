9505c1a52abad0665108c65fdd3a1619a0c70caf
Nicholas Cunningham <ndcunningham@gmail.com>
2023-07-19 14:56:16 -0600

    feat(svelte): Add svelte example recipe

---
+++ svelte/apps/svelteapp/src/App.svelte
@@ -0,0 +1,33 @@
+ <script lang="ts">
+	import { Counter } from '@svelte/shared';
+	export let name: string;
+</script>
+
+<main>
+	<h1>Welcome {name}!</h1>
+	<p>Visit <a href="https://nx.dev">Nx</a> or <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<Counter/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
