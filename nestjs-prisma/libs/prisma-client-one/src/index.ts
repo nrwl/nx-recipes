9869d494ebf94980f8676b7ded81d0deeb71ab28
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2023-07-19 13:50:25 +0000

    chore: update shutdown hooks references

--- nestjs-prisma/libs/prisma-client-one/src/index.ts
+++ nestjs-prisma/libs/prisma-client-one/src/index.ts
@@ -1,13 +1,3 @@
-import { INestApplication } from '@nestjs/common';
-import { PrismaService } from './lib/prisma.service';
-
 export * from './lib/prisma-client-one.module';
 export * from './lib/prisma.service';
 export { User, Post, Prisma } from '@prisma/client/one';
-
-export async function registerPrismaShutdown(app: INestApplication) {
-  // recommended by NestJS
-  // https://docs.nestjs.com/recipes/prisma#issues-with-enableshutdownhooks
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
-}
