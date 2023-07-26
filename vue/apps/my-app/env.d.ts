23c7e4c6421931b2e7ebcf5710a58f5d9743fa6c
Nicholas Cunningham <ndcunningham@gmail.com>
2023-07-25 19:43:12 -0600

    feat(vue): add vue recipe (#44)

---
+++ vue/apps/my-app/env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+/* eslint-disable */
+declare module '*.vue' {
+import type { DefineComponent } from 'vue';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const component: DefineComponent<object, object, any>;
+export default component;
+}
---
+++ vue/libs/icons/README.md
@@ -0,0 +1,11 @@
+# icons
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build icons` to build the library.
+
+## Running unit tests
+
+Run `nx test icons` to execute the unit tests via [Jest](https://jestjs.io).
