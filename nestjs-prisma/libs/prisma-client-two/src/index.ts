c6431751145fa2968495dfd5451e8a54ba769412
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2023-07-28 17:11:07 +0200

    chore: remove shutdown hooks (#45)

--- nestjs-prisma/libs/prisma-client-two/src/index.ts
+++ nestjs-prisma/libs/prisma-client-two/src/index.ts
@@ -1,13 +1,3 @@
-import { INestApplication } from '@nestjs/common';
-import { PrismaService } from './lib/prisma.service';
-
 export * from './lib/prisma-client-two.module';
 export * from './lib/prisma.service';
 export { Prisma, Item } from '@prisma/client/two';
-
-export async function registerPrismaShutdown(app: INestApplication) {
-  // recommended by NestJS
-  // https://docs.nestjs.com/recipes/prisma#issues-with-enableshutdownhooks
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
-}
