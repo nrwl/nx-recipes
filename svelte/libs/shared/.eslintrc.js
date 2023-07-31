a7179ccf9099a0b353be8e7cbdf255f265bd2610
Nicholas Cunningham <ndcunningham@gmail.com>
2023-07-31 02:21:26 -0600

    feat(svelte): Add svelte example recipe (#46)

---
+++ svelte/libs/shared/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['svelte3', '@typescript-eslint'],
+  extends: ['../../.eslintrc.json'],
+  ignorePatterns: ['!**/*', 'vite.config.ts'],
+  overrides: [
+    {
+      files: ['*.ts', '*.js', '*.svelte'],
+      rules: {},
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {},
+    },
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {},
+    },
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  settings: {
+    'svelte3/typescript': require('typescript'),
+  },
+};
