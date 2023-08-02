91f66a95375fad6a4a8aec62f3c47e13164097df
Caleb Ukle <caleb.ukle+github@pm.me>
2023-08-02 08:44:19 -0500

    setup configs for nuxt

--- nuxt-integrated/apps/demo/app.vue
+++ nuxt-integrated/apps/demo/app.vue
@@ -1,5 +1,14 @@
+<script setup lang="ts">
+
+const count = ref(0);
+const incr = () => count.value++;
+const dec = () => count.value--;
+</script>
 <template>
   <div>
-    <NuxtWelcome />
+    <button v-on:click="dec">-</button>
+    <button v-on:click="incr">+</button>
+
+    {{ count }}
   </div>
 </template>
