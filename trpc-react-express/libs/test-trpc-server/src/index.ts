be688379e89996957cd7aa2b1839b696a7555429
quirkles <al.quirk@gmail.com>
2023-08-07 21:13:52 -0400

    repro bug

--- trpc-react-express/libs/test-trpc-server/src/index.ts
+++ trpc-react-express/libs/test-trpc-server/src/index.ts
@@ -1,10 +1,17 @@
 import { initTRPC } from '@trpc/server';
+import {z} from "zod"
+
+const inputSchema = z.object({
+  username: z.string()
+})
+  // .optional()
+// add/remove this .optional() call and see the ts error in trpc-react-express/apps/test-web/src/app/app.tsx
 
 const t = initTRPC.create();
 
 export const trpcRouter = t.router({
-  welcomeMessage: t.procedure.query((req) => ({
-    welcomeMessage: `Welcome to test!`,
+  welcomeMessage: t.procedure.input(inputSchema).query((req) => ({
+    welcomeMessage: `Hi ${req.input?.username || 'User'}. Welcome to test!`,
   })),
 });
 
