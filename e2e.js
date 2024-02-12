7204da2b936afc364bd7c6d912e0a7617fb341e1
Isaac Mann <isaacplmann@gmail.com>
2024-02-12 15:18:00 -0500

    fix e2e

--- e2e.js
+++ e2e.js
@@ -50,7 +50,7 @@ function installPackages(cwd) {
     execSync("yarn", { cwd });
   } else {
     checkLockfileForLocalhost(`${cwd}/package-lock.json`)
-    execSync("npm i --legacy-peer-deps", { cwd });
+    execSync("npm ci --legacy-peer-deps", { cwd });
   }
 }
 
