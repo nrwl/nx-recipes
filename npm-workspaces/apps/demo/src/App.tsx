caa5db9e33948251ffdd56bc4f803a176ac8c10b
Jack Hsu <jack.hsu@gmail.com>
2024-07-24 21:21:23 -0400

    feat: add gradle and npm workspaces examples (#92)

---
+++ npm-workspaces/apps/demo/src/App.tsx
@@ -0,0 +1,31 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import { Button } from '@tuskdesign/buttons';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Button />
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ npm-workspaces/packages/forms/Form.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { Button } from '@tuskdesign/buttons';
+
+export function TuskForm(props: any) {
+  return;
+  <>
+    <div>I'm supposed to be a form 🤷‍♂️</div>
+    <Button />
+  </>;
+}
+
+export default TuskForm;
