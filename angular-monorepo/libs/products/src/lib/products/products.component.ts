ea59bf547c48b65aaf2fe55fd519402360e2e7fd
Isaac Mann <isaacplmann@gmail.com>
2025-05-05 11:31:59 -0400

    update nx version for tutorials

--- angular-monorepo/libs/products/src/lib/products/products.component.ts
+++ angular-monorepo/libs/products/src/lib/products/products.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'angular-monorepo-products',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.css',
+    selector: 'angular-monorepo-products',
+    imports: [CommonModule],
+    templateUrl: './products.component.html',
+    styleUrl: './products.component.css'
 })
 export class ProductsComponent {}
